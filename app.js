05f58d6d6a3fdeae83d47c76b765a7897928e38f
gialli24 <a.gialli2409@gmail.com>
2026-04-09 12:40:58 +0200

    add internal server errro 500 middleware

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 /* Middlewares */
+const errorHandler = require("./middlewares/errorHandler");
 const notFound = require("./middlewares/notFound");
 
-app.use(notFound);
 
 /* Body Parser */
 app.use(express.json());
@@ -16,6 +16,10 @@ app.use("/posts", postsRouter);
 
 app.use(express.static('public'));
 
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}, http://localhost:${port}`)
 });
+
+app.use(errorHandler);
+app.use(notFound);
