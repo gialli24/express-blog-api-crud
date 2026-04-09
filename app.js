4a325303c13411b3b134d8eba679871c30babdde
gialli24 <a.gialli2409@gmail.com>
2026-04-09 12:29:03 +0200

    add not found 404 middleware

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+/* Middlewares */
+const notFound = require("./middlewares/notFound");
+
+app.use(notFound);
+
 /* Body Parser */
 app.use(express.json());
 
@@ -12,5 +17,5 @@ app.use("/posts", postsRouter);
 app.use(express.static('public'));
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+    console.log(`Listening on port ${port}, http://localhost:${port}`)
 });
